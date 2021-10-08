6d66b08e5d984fa46df220078b10741026ad52a8
Jayson Santiago <jayson.santiago1216@gmail.com>
2021-10-08 14:36:17 -0600

    Add Change password to Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 import { AuthComponent } from './auth/auth.component';
 import { SignupComponent } from './auth/components/signup/signup.component';
+import { ChangePasswordComponent } from './auth/components/change-password/change-password.component';
 
 const routes: Routes = [
   { path: 'about-us', component: AboutUsComponent },
   { path: 'how-it-works', component: HowItWorksComponent },
   {
     path: 'auth', component: AuthComponent, children: [
-      { path: 'sign-up', component: SignupComponent }
+      { path: 'sign-up', component: SignupComponent },
+      { path: 'change-password', component: ChangePasswordComponent }
     ]
   }];
 
